14263c5d0df000456cc34d8f522f4e76cc1a9adc
박찬익 <piaocanyi@bagchan-igdeMacBook-Pro.local>
2022-11-28 19:56:58 -0800

    돔을 이용하여 html 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,21 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+const id = document.querySelector("#id")
+const pw = document.querySelector("#pw")
+const loginBtn  = document.querySelector("button")
+
+
+//console.log(id); defer
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    // console.log("login클릭")
+    const req  = { 
+        id : id.value,
+        pw : pw.value,
+    };
+
+    console.log(req);
+}
+
